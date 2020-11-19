5c7e191cddddb97bb663c893d00ecb3d001f4547
heenahmed <66954825+heenahmed@users.noreply.github.com>
2020-11-19 19:31:35 +0530

    Create PA-3.py

---
+++ week-3/PA-3.py
@@ -0,0 +1,16 @@
+a = int(input())
+count=0
+list_1 = []
+
+for i in range(1,51):
+  list_1.append(i)
+  
+for num in list_1:
+  if(num==a):
+    continue
+  else:
+    if(num%a == 0):
+      count = count +1
+    
+    
+print(count,end="")
