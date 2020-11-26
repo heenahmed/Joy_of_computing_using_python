1b9d8511b7f15140f859732df6261f3cf9936886
heenahmed <66954825+heenahmed@users.noreply.github.com>
2020-11-26 21:42:37 +0530

    Update PA-3.py

--- week-11/PA-3.py
+++ week-11/PA-3.py
@@ -1,4 +1,4 @@
-num = input()
+num = input() #passed and submitted
 num = num.split(",")
 
 m = num[0]
@@ -12,3 +12,4 @@ for i in range(f, e+1):
     if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0 ):
         items.append(s)
 print( ",".join(items),end="")
+    
