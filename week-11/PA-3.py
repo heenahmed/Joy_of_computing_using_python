8cecedc97a54564a6481edd5aeaba6362664b9d3
heenahmed <66954825+heenahmed@users.noreply.github.com>
2020-11-26 21:41:40 +0530

    Create PA-3.py

---
+++ week-11/PA-3.py
@@ -0,0 +1,14 @@
+num = input()
+num = num.split(",")
+
+m = num[0]
+n = num[1]
+f=int(num[0])
+e=int(num[1])
+
+items = []
+for i in range(f, e+1):
+    s = str(i)
+    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0 ):
+        items.append(s)
+print( ",".join(items),end="")
