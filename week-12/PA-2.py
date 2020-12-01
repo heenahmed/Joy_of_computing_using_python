3a1bd654abbb97e704b54d9c006e4d026a4a9069
heenahmed <66954825+heenahmed@users.noreply.github.com>
2020-12-01 20:58:07 +0530

    Update PA-2.py

--- week-12/PA-2.py
+++ week-12/PA-2.py
@@ -1,13 +1,11 @@
-s = input() #second test case not passed
+s = input() # passed and submitted
 u=0
 l=0
-sp =  '`~!@#$%^&*()_-+=|\}]{[":;?/>.<,'
+
 for i in s:
-  #print('i is'+str(i))
-  if(i == i.upper() and i != " " and i not in sp  and i !="'"):
+  if(i==i.upper() and i.isalpha()==True):
     u+=1
-    #print('u is '+str(u))
-  elif(i !=" " and i not in sp and i !="'"):
+  elif(i==i.lower() and i.isalpha()==True):
     l+=1
-    #print('l is '+str(l))
+  
 print(str(u)+' '+str(l),end="")
